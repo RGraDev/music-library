4c4f76e39b230d4167ac07728cb39dcda15b26cb
Ryan Granger <ryan.granger98@gmail.com>
2023-09-11 21:43:37 +0100

    DB migrations, scripts, and controller/route to add artists to the artists table

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const artistRouter = require('./routes/artist');
+const app = express();
+app.use(express.json());
+app.use(artistRouter);
+
+app.get('/', (req, res) => {
+  res.status(200).json({ result: 'Hello World' });
+})
+
+module.exports = app;
